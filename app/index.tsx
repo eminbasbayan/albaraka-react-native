26f6ace74975fe93b915da5985f475fdd70ef080
Emin Basbayan <emin.basbayan@icloud.com>
2025-06-24 14:54:51 +0300

    fetch products

--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,69 @@
+import { useState } from 'react';
+import { Alert, FlatList, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 import Button from '@/components/Button';
-import { productsData } from '@/constants/data';
-import { View } from 'react-native';
+import ProductCard from '@/components/ProductCard';
 
 export default function Index() {
-  console.log(productsData);
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchProducts = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch('https://fakestoreapi.com/products/');
+
+      if (!res.ok) {
+        throw new Error('Response ok değil!');
+      }
+
+      const data = await res.json();
+      setProducts(data);
+    } catch (err) {
+      Alert.alert(
+        'Hata',
+        'Ürünler Yüklenirken hata oluştu. Lütfen tekrar deneyin!'
+      );
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Button
-        title={'Sepete Ekle'}
-        onPress={() => console.log('Sepete Eklendi!')}
-        loading={true}
-      />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 24,
+            fontWeight: 'bold',
+            textAlign: 'center',
+          }}
+        >
+          Ürünlerimiz
+        </Text>
 
-      <Button title="İptal" onPress={() => {}} variant="secondary" />
-
-      <Button title="Detaylar" onPress={() => {}} variant="outline" />
-      <Button title="Satın Al" onPress={() => {}} disabled={true} />
-
-      <Button title="Giriş Yap" onPress={() => {}} fullWidth />
-
-      {/* <FlatList
-       data={productsData}
-       keyExtractor={(item, index)=> index.toString()}
-       showsVerticalScrollIndicator={false}
-       renderItem={({item})=> {
-        return <ProductCard key={item.id} {...item} />
-       }}
-      /> */}
-    </View>
+        <Button title="Ürünleri Getir" onPress={fetchProducts} />
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : (
+          <FlatList
+            data={products}
+            keyExtractor={(item, index) => index.toString()}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => {
+              return <ProductCard key={item.id} {...item} />;
+            }}
+          />
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
